03d90c1e6caa14a0c585bd9a97ba8e607742410a
felippeissa <contato.issafe@gmail.com>
2019-09-10 15:03:31 -0300

    Programa Funcional

--- frontend/AirBnbApp/src/pages/signIn/index.js
+++ frontend/AirBnbApp/src/pages/signIn/index.js
@@ -1,30 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { StatusBar, AsyncStorage } from 'react-native';
+import { StackActions, NavigationActions } from 'react-navigation';
+
+import api from '../../services/api';
+
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text>
-        Ola mundo
-      </Text>
-    </View>
-  );
-};
+  Container,
+  Logo,
+  Input,
+  ErrorMessage,
+  Button,
+  ButtonText,
+  SignUpLink,
+  SignUpLinkText,
+} from './styles';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontSize: 12,
-    backgroundColor: '#F5FCFF',
-  },
-});
+export default class SignIn extends Component {
+  static navigationOptions = {
+    header: null,
+  };
+  handleEmailChange = (email) => {
+    this.setState({ email });
+  };
+  
+  handlePasswordChange = (password) => {
+    this.setState({ password });
+  };
+  
+  handleCreateAccountPress = () => {
+    this.props.navigation.navigate('SignUp');
+  };
+  handleSignInPress = async () => {
+    if (this.state.email.length === 0 || this.state.password.length === 0) {
+      this.setState({ error: 'Preencha usuário e senha para continuar!' }, () => false);
+    } else {
+      try {
+        const response = await api.post('/sessions', {
+          email: this.state.email,
+          password: this.state.password,
+        });
+          
+        await AsyncStorage.setItem('@AirBnbApp:token', response.data.token);
 
-export default App;
+        const resetAction = StackActions.reset({
+          index: 0,
+          actions: [
+            NavigationActions.navigate({ routeName: 'Main' }),
+          ],
+        });
+        this.props.navigation.dispatch(resetAction);
+      } catch (_err) {
+        this.setState({ error: 'Houve um problema com o login, verifique suas credenciais!' });
+      }
+    }
+  };
+  state = { email: '', password: '', error: '' };
+  render() {
+    return (
+      <Container>
+        <StatusBar hidden />
+        <Logo source={require('../../images/airbnb_logo.png')} resizeMode="contain" />
+        <Input
+          placeholder="Endereço de e-mail"
+          value={this.state.email}
+          onChangeText={this.handleEmailChange}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Input
+          placeholder="Senha"
+          value={this.state.password}
+          onChangeText={this.handlePasswordChange}
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry
+        />
+        {this.state.error.length !== 0 && <ErrorMessage>{this.state.error}</ErrorMessage>}
+        <Button onPress={this.handleSignInPress}>
+          <ButtonText>Entrar</ButtonText>
+        </Button>
+        <SignUpLink onPress={this.handleCreateAccountPress}>
+          <SignUpLinkText>Criar conta grátis</SignUpLinkText>
+        </SignUpLink>
+      </Container>
+    );
+  }
+}
